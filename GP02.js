c61d0d0e4b163db711c97f42ef688478ce8f1b54
YASAI <yasai.vegitable@gmail.com>
2013-01-27 03:59:57 +0900

    Fix tab

--- GP02.js
+++ GP02.js
@@ -1,8 +1,8 @@
 var GP02 = (function() {
-    GP02.prototype.server = "";
+		GP02.prototype.server = "";
 
-    // 基本的な情報取得API
-    GP02.prototype.api = {
+		// 基本的な情報取得API
+		GP02.prototype.api = {
 		status: 'monitoring/status',
 		traffic_status: 'monitoring/traffic-statistic',
 		connection: 'dialup/connection',
@@ -10,44 +10,44 @@ var GP02 = (function() {
 		language: 'language/current-language',
 		pinStatus: 'pin/status',
 		plmn: 'net/current-plmn'
-    };
+		};
 
-    function GP02(server) {
+		function GP02(server) {
 		this.server = server || '192.168.1.1';
-    }
+		}
 
-    GP02.prototype.request = function(api, callback) {
+		GP02.prototype.request = function(api, callback) {
 		$.ajax({
-		    type: 'GET',
-		    url: './request.php',
-		    data: {
-		    	api: api,
-		    	server: this.server
-		   	},
-		    success: callback
+				type: 'GET',
+				url: './request.php',
+				data: {
+					api: api,
+					server: this.server
+			 	},
+				success: callback
 		});
-    };
+		};
 
-    GP02.prototype.getStatus = function(callback) {
+		GP02.prototype.getStatus = function(callback) {
 		this.request(this.api.status, callback);
-    };
-    GP02.prototype.getTrafficStatus = function(callback) {
+		};
+		GP02.prototype.getTrafficStatus = function(callback) {
 		this.request(this.api.traffic_status, callback);
-    };
-    GP02.prototype.getConnection = function(callback) {
+		};
+		GP02.prototype.getConnection = function(callback) {
 		this.request(this.api.connection, callback);
-    };
-    GP02.prototype.getProfiles = function(callback) {
+		};
+		GP02.prototype.getProfiles = function(callback) {
 		this.request(this.api.profiles, callback);
-    };
-    GP02.prototype.getLanguage = function(callback) {
+		};
+		GP02.prototype.getLanguage = function(callback) {
 		this.request(this.api.language, callback);
-    };
-    GP02.prototype.getPinStatus = function(callback) {
+		};
+		GP02.prototype.getPinStatus = function(callback) {
 		this.request(this.api.pinStatus, callback);
-    };
-    GP02.prototype.getPlmn = function(callback) {
+		};
+		GP02.prototype.getPlmn = function(callback) {
 		this.request(this.api.plmn, callback);
-    };
-    return GP02;
+		};
+		return GP02;
 })();
